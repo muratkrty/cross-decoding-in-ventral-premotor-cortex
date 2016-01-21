f8679e2d9e41482df76ce4b52cb05b693cd9cd20
muratkrty <muratkrty@gmail.com>
2016-01-21 18:15:43 +0100

    Added extract extractEventsData.m

---
+++ extractEventsData.m
@@ -0,0 +1,12 @@
+function [ movementData, graspingData ] = extractEventsData( bestUnits, movementInd, graspingInd )
+% Author: Murat Kirtay, The BioRobotics Inst.
+% Date: 21/01/2016
+% Description: Extract onmove and grasping event data for specified index
+%              range
+% Notes: 0- Data vectors are raw spiking data. 
+%        1- Window size should be performed to decrease resolution
+
+
+
+end
+
