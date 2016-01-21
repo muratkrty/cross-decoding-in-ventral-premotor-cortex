2b25d8f84da91afc1ba314e233519ed9815be8cb
muratkrty <muratkrty@gmail.com>
2016-01-21 16:53:21 +0100

    Added extractBestUnits.m

---
+++ extractBestUnits.m
@@ -0,0 +1,31 @@
+function [ bestUnits ] = extractBestUnits( units )
+% Author: Murat Kirtay, The BioRobotics Inst.
+% Date: 21/01/2016
+% Description: Extract best units from available data set
+% Notes: 0- Object fixation experiment excluded.
+
+experimentIDs = [1 2 4]; % no obj-fix
+objectIDs = [1:4];
+
+emptyExpUnit = []
+for j=units,
+    
+    unitFileName = setUnitName(j);
+     
+    unitStr = strcat('~/DATA/', unitFileName);
+    unit = load(unitStr{1,1})
+        
+    for i=experimentIDs,
+       for ii=objectIDs,
+           if isempty(unit.data.spikes{i, ii}) 
+               emptyExpUnit = [emptyExpUnit j]
+           end
+       end
+    end
+       
+end
+
+bestUnits = setdiff(units, unique(emptyExpUnit))
+
+end
+
