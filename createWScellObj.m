a821ea6466c00304f082f7e21c2cf22b3ad20809
muratkrty <muratkrty@gmail.com>
2016-01-22 12:12:32 +0100

    Added createWScellObj.m

---
+++ createWScellObj.m
@@ -0,0 +1,19 @@
+function [ wSCell ] = createWScellObj( data, winStep )
+% Author: Murat Kirtay, The BioRobotics Inst.
+% Date: 22/01/2016
+% Description: Crate window sized data with object ids.
+% Notes: 0- last column consists of object ids.
+
+experimentIDs = [1 2 4]; % exclude obj-fixation
+objectIDs = [1:4];
+
+windowSize = winStep(1);
+stepSize = winStep(2);
+
+
+
+
+
+
+end
+
