fd870578762198e0286e31ebc21bf0c8fa94a91c
muratkrty <muratkrty@gmail.com>
2016-01-22 17:59:04 +0100

    Added extractRegParams.m

---
+++ extractRegParams.m
@@ -0,0 +1,12 @@
+function [ w, yPred, errorTraining ] = extractRegParams( x, y)
+% Author: Murat Kirtay, Robotics Laboratory
+% Date: 22/01/2016
+% Description: get training weight, prediction vector and 
+%              training errors for specifc unit.
+
+w = pinv(x, y);
+yPred = x * w;
+errorTraining = abs(y-yPred);
+
+end
+
