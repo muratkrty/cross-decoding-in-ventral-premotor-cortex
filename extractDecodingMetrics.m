f2b128c6982d6906b5f62d56533ff9ff91af176a
muratkrty <muratkrty@gmail.com>
2016-01-23 15:20:16 +0100

    Added decoding performance metric

---
+++ extractDecodingMetrics.m
@@ -0,0 +1,17 @@
+function [ absMetric, relMetric ] = extractDecodingMetrics(decTreshold, testErr, Y)
+% Author: Murat Kirtay, The BioRobotics Inst.
+% Date: 23/01/2016
+% Description: Extract decoding performance metrics values for each
+%              object based on absolute and relative decoding 
+%              characteristics
+% Notes: 0- High absolute specifity is high engagement with an object
+
+    testSize = size(testErr, 1);
+    objLen = testSize/objSize;
+    
+ 
+
+
+
+end
+
