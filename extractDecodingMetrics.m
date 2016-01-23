016240c83eecb29584cc4c85c62bba565244324a
muratkrty <muratkrty@gmail.com>
2016-01-23 15:29:39 +0100

    Absolute/relative specifity added

--- extractDecodingMetrics.m
+++ extractDecodingMetrics.m
@@ -6,12 +6,21 @@ function [ absMetric, relMetric ] = extractDecodingMetrics(decTreshold, testErr,
 %              characteristics
 % Notes: 0- High absolute specifity is high engagement with an object
 
+    objectIDs = [1:4];
+    objSize = size(objectIDs,2);
     testSize = size(testErr, 1);
     objLen = testSize/objSize;
-    
- 
 
-
+    % below trick will save you from loops
+    objTest = reshape(testErr', objLen, objSize)';
+    absMetric = [length(find(objTest(1, :) <= decTreshold)),
+                 length(find(objTest(2, :) <= decTreshold)),
+                 length(find(objTest(3, :) <= decTreshold)),
+                 length(find(objTest(4, :) <= decTreshold))];
+   
+    % extract absolute/relative perfomance metric values for each object
+    relMetric = 100/testSize * absMetric';
+    absMetric = 100/objLen * absMetric';
 
 end
 
