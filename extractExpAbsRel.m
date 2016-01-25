0981b3a3e10121452b18688e0b7807e5816abae1
muratkrty <muratkrty@gmail.com>
2016-01-25 11:48:41 +0100

    Added extractExpAbsRel to get absolute and relative matrices

---
+++ extractExpAbsRel.m
@@ -0,0 +1,14 @@
+function [ unitAbsMetrics, unitRelMetrics] = extractExpAbsRel( mCell, id,...
+                                             experiment, winStep, decTreshold)
+% Author: Murat Kirtay, Robotics Laboratory
+% Date: 25/1/2016
+% Description: Extract ranked unit ids based on absolute/relative 
+%              specifity matrices. Then, combine them for latex table.
+% Note: 0- this will be ugliest function.
+
+
+
+
+
+end
+
